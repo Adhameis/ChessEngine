99fdc0b4958d6b34e6a94ed15223e92ec57443dc
eyad-fn17 <eyadeissa8@gmail.com>
2025-12-31 18:40:40 +0000

    Update chessgame.py

--- chessgame.py
+++ chessgame.py
@@ -1,13 +1,15 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 ROWS = 8
 COLUMNS = 8
 
 
 class pieces():
-    def __init__(self,color,x,y):
+    def __init__(self,color,x,y, piece_type):
         self.color = color
         self.coordinates = [x,y]
+        self.type = piece_type
     def Move(self,x,y):
         self.coordinates = [x,y]
 
@@ -15,38 +17,38 @@ class pieces():
 
 class Queen(pieces):
     def __init__(self,color,x,y):
-        super().__init__(color,x,y)
+        super().__init__(color,x,y,'Queen')
 
 
 
 class King(pieces):
     def __init__(self,color,x,y):
-          super().__init__(color,x,y)
+          super().__init__(color,x,y,'King')
 
 
 
 class pawn(pieces):
     def __init__(self,color,x,y):
-        super().__init__(color,x,y)
+        super().__init__(color,x,y,'Pawn')
 
 
 
 class bishop(pieces): 
     def __init__(self,color,x,y):
-        super().__init__(color,x,y)
+        super().__init__(color,x,y,'Bishop')
 
 
 
 
 class knight(pieces): 
     def __init__(self,color,x,y):
-        super().__init__(color,x,y)
+        super().__init__(color,x,y,'Knight')
 
 
 
 class rook(pieces): 
     def __init__(self,color,x,y):
-        super().__init__(color,x,y)
+        super().__init__(color,x,y,'Rook')
 
 
 
@@ -76,23 +78,39 @@ def setup_pieces():
         pieces_on_board.append(pawn('white',i,7))
         pieces_on_board.append(pawn('black',i,2))
     for p in pieces_on_board:
-        label = Label(window,text=p.coordinates,fg=p.color)
-        label.grid(column=int(p.coordinates[0])-1,row=int(p.coordinates[1])-1)
+        if (int(p.coordinates[0]) + int(p.coordinates[1])) % 2 == 0:
+            square_color='white'
+        else:
+            square_color="grey"
+
+        if p.color == 'black':
+            c='b'
+        else:
+            c='w'
+        path =f'/home/eyad/Chess_Pieces/{p.type.lower()}-{c}.png'
+
+        og_img = Image.open(path).convert('RGBA')
+        resized_img = og_img.resize((80, 80))
+        img = ImageTk.PhotoImage(resized_img)
+        panel = Label(window, image=img,bg=square_color)
+        panel.image = img
+        panel.grid(column=int(p.coordinates[0])-1,row=int(p.coordinates[1])-1)
 
 
 def draw_board():
     for row in range(ROWS):
         for column in range(COLUMNS):
             if (row + column) % 2 == 0:
-                color='grey'
+                color='white'
             else:
-                color="black"
+                color="grey"
 
             canvas = Canvas(window,bg=color,width=80,height=80)
             canvas.grid(row=row,column=column)
     setup_pieces()
 
 window = Tk()
+window.title('Chess')
 window.geometry('640x640')
 window.config(background='white')
 draw_board()
